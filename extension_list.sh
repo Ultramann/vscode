ebd1b5be9ac2de03fa6d03565ff13044ef55cd5d
Cary Goltermann <cary.goltermann@gmail.com>
2023-01-06 09:05:42 -0600

    Filter empty identity strings when building extension list

--- extension_list.sh
+++ extension_list.sh
@@ -2,7 +2,8 @@ identityre='(.*)-[0-9]'
 for dir in $(ls extensions); do
     [[ $dir =~ $identityre ]]
     identity=${BASH_REMATCH[1]}
-    if [ $identity = "ultra.cmg" ]; then
+    if [ "${identity}" = "" ] || [ "${identity}" = "ultra.cmg" ]
+    then
         continue
     fi
     echo $identity
